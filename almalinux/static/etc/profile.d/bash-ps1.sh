dc2df6326b828f487a7cf90492a9fafcdf55ee44
Allen Conlon <software@conlon.dev>
2025-05-04 20:27:33 -0400

    chore(alma): update logic

--- almalinux/static/etc/profile.d/bash-ps1.sh
+++ almalinux/static/etc/profile.d/bash-ps1.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-if (( $UID > 1000 )); then
+if (( $UID > 999 )); then
   export PS1='[\[\e[93m\]\u\[\e[97m\]@\h:\[\e[36m\] \W\[\e[0m\]]: '
 else
   export PS1='[\[\e[91m\]\u\[\e[97m\]@\h:\[\e[36m\] \W\[\e[0m\]]: '
