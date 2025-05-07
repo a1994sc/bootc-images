32dcc84b6d94126e52fb0a09857aadbba2381c91
Allen Conlon <software@conlon.dev>
2025-05-07 19:54:13 -0400

    feat: update profile

--- almalinux/static/etc/profile.d/bash-ps1.sh
+++ almalinux/static/etc/profile.d/bash-ps1.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 if (( $UID > 999 )); then
-  export PS1='[\[\e[93m\]\u\[\e[97m\]@\h:\[\e[36m\] \W\[\e[0m\]]: '
+  export PS1='[\u@\h:\[\e[36m\] \W\[\e[0m\]]: '
 else
   export PS1='[\[\e[91m\]\u\[\e[97m\]@\h:\[\e[36m\] \W\[\e[0m\]]: '
 fi
